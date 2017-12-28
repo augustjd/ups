b63157e13d9343fa5a56a15c7bb1cecd0a4f14b0
Joshua Augustin <augustinspring@gmail.com>
2017-12-28 10:49:03 -0800

    Adds run.py.

---
+++ run.py
@@ -0,0 +1,4 @@
+from ups import create_app
+
+
+app = create_app("development")
--- ups/views/release_views.py
+++ ups/views/release_views.py
@@ -1,35 +1,57 @@
 from flask import request
 
-from .responses import ReleaseNotFoundErrorResponse
+from .responses import ReleaseNotFoundErrorResponse, SuiteNotFoundErrorResponse
 from .blueprint import blueprint
 
-from ups.models import (Release, release_manifest_schema)
+from ups.models import (Release, release_manifest_schema, Suite)
 
 from isodate import parse_datetime
 
 
-@blueprint.route('/releases/<id>', methods=['GET'])
-def route_get_release(id):
-    if id == 'current':
-        release = Release.current()
-    else:
-        release = Release.get(id)
+def get_release(id):
+    release = Release.get(id)
 
     if release is None:
         raise ReleaseNotFoundErrorResponse(id)
+
+    return release
+
+
+@blueprint.route('/releases/<id>', methods=['GET'])
+def route_get_release(id):
+    release = get_release(id)
+
+    return release_manifest_schema.jsonify(release)
+
+
+@blueprint.route('/releases/', methods=['POST'])
+def route_create_release(id):
+    d = request.get_json()
+
+    title = None
+    suite = None
+
+    if isinstance(d, dict):
+        if isinstance(d.get('title'), str):
+            title = d['title']
+
+        if isinstance(d.get('suite_id'), str):
+            suite_id = d['suite_id']
+            suite = Suite.get(suite_id)
+
+            if suite is None:
+                raise SuiteNotFoundErrorResponse(suite_slug=suite_id)
+
+    release = Release(title=title, suite=suite)
+
+    release.save()
 
     return release_manifest_schema.jsonify(release)
 
 
 @blueprint.route('/releases/<id>/schedule', methods=['POST'])
 def route_schedule_release(id):
-    if id == 'current':
-        release = Release.current()
-    else:
-        release = Release.get(id)
-
-    if release is None:
-        raise ReleaseNotFoundErrorResponse(id)
+    release = get_release(id)
 
     datetime = parse_datetime(request.get_json().get('datetime'))
 
